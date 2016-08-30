61a535eb01a54943ec8ee4544626dd6ced244672
Ryan Mills <Ryan.Mills@sophos.com>
2016-08-30 17:09:32 +0100

    Attempt at using JSONDoc

--- src/main/java/com/mills/organiser/controllers/OrganisationController.java
+++ src/main/java/com/mills/organiser/controllers/OrganisationController.java
@@ -3,6 +3,9 @@ package com.mills.organiser.controllers;
 import com.mills.organiser.models.Organisation;
 import com.mills.organiser.repositories.OrganisationRepository;
 import org.apache.commons.lang3.RandomUtils;
+import org.jsondoc.core.annotation.*;
+import org.jsondoc.core.pojo.ApiStage;
+import org.jsondoc.core.pojo.ApiVisibility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,9 @@ import java.util.List;
 /**
  * Created by ryanmills on 23/08/2016.
  */
+@Api(name="organisations", visibility = ApiVisibility.PUBLIC, stage = ApiStage.GA, description = "Methods for viewing and manipulating organisations")
+@ApiVersion(since = "1.0")
+@ApiAuthNone
 @RestController
 @RequestMapping("/organisations")
 public class OrganisationController {
@@ -22,9 +28,14 @@ public class OrganisationController {
     @Autowired
     OrganisationRepository _organisationRepository;
 
+    @ApiMethod
     @RequestMapping
-    public Organisation getOrganisations() {
-        return _organisationRepository.findAll().iterator().next();
+    public @ApiResponseObject List<Organisation> getOrganisations() {
+        List<Organisation> organisations = new ArrayList<>();
+        for(Organisation organisation : organisations) {
+            organisations.add(organisation);
+        }
+        return organisations;
     }
 
     @RequestMapping(method = RequestMethod.PUT)
