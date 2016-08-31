775f79221dba69bd619b5014ab381b00758156dd
Ryan Mills <rem518@york.ac.uk>
2016-08-31 21:20:13 +0100

    Add a base path for the API

--- src/main/java/com/mills/organiser/repositories/OrganisationRepository.java
+++ src/main/java/com/mills/organiser/repositories/OrganisationRepository.java
@@ -2,10 +2,12 @@ package com.mills.organiser.repositories;
 
 import com.mills.organiser.models.Organisation;
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by ryanmills on 23/08/2016.
  */
+@Service
 public interface OrganisationRepository extends GraphRepository<Organisation> {
 
 }
